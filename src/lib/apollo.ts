6494970c4436db4e1f9053772a778ed3bea3650d
Daniel Conceição <danieldcc.colares@gmail.com>
2022-06-26 12:18:58 -0300

    ✨ create user mutation

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4rrt4fe1hrq01wgfjmdh04u/master",
+  uri: import.meta.env.VITE_API_URL,
+
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
